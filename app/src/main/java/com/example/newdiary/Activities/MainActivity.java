16af21b33b17bef17d00c818008e9be88938e5a8
Olivia Xie <oxie@bdo.ca>
2019-08-06 15:49:40 -0400

    second commit

--- app/src/main/java/com/example/newdiary/Activities/MainActivity.java
+++ app/src/main/java/com/example/newdiary/Activities/MainActivity.java
@@ -1,17 +1,31 @@
 package com.example.newdiary.Activities;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
 import com.example.newdiary.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
+
+    private AlertDialog.Builder alertDialogBuilder;
+    private AlertDialog dialog;
+
+    private String selectedDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +38,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                showInputDialog();
             }
         });
     }
@@ -50,5 +63,31 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void showInputDialog() {
+
+        alertDialogBuilder = new AlertDialog.Builder(this);
+        View view = getLayoutInflater().inflate(R.layout.dialog_view, null);
+
+        Button okButton = view.findViewById(R.id.okButtonId);
+        final CalendarView calendarView = view.findViewById(R.id.calendarId);
+
+        alertDialogBuilder.setView(view);
+        dialog = alertDialogBuilder.create();
+        dialog.show();
+
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+                selectedDate = sdf.format(new Date(calendarView.getDate()));
+                Intent intent = new Intent(MainActivity.this, NewEntryActivity.class);
+                intent.putExtra("date", selectedDate);
+                startActivity(intent);
+            }
+        });
+
     }
 }
