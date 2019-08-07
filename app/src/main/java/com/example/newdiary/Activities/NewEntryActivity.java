dd056e21a4f5d04aec7faf15e1b9c7f13ec53cd3
Olivia Xie <olivia.xie@hotmail.com>
2019-08-06 23:56:17 -0400

    progress

--- app/src/main/java/com/example/newdiary/Activities/NewEntryActivity.java
+++ app/src/main/java/com/example/newdiary/Activities/NewEntryActivity.java
@@ -1,15 +1,25 @@
 package com.example.newdiary.Activities;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.newdiary.Data.DatabaseHandler;
+import com.example.newdiary.Models.Entry;
 import com.example.newdiary.R;
 
 public class NewEntryActivity extends AppCompatActivity {
+
+    private String entryDate;
+    private TextView dateTextView;
+    private EditText titleEditText;
+    private EditText entryEditText;
+    private DatabaseHandler dbHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +28,65 @@ public class NewEntryActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        entryDate = getIntent().getStringExtra("date");
 
+        dateTextView = findViewById(R.id.entryDateId);
+        dateTextView.setText(entryDate);
+
+        titleEditText = findViewById(R.id.titleEditTextId);
+        entryEditText = findViewById(R.id.entryEditTextId);
+
+        dbHandler = new DatabaseHandler(NewEntryActivity.this);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+
+    }
+
+    // Opening Search alert dialog from menu Search button
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.save_entry) {
+
+            Toast.makeText(getApplicationContext(), "save button clicked.", Toast.LENGTH_LONG).show();
+            saveEntryToDb();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void saveEntryToDb() {
+
+        Entry entry = new Entry();
+        String title = titleEditText.getText().toString().trim();
+        String text = entryEditText.getText().toString().trim();
+
+        if (title.length() == 0 || text.length() == 0) {
+
+            Toast.makeText(getApplicationContext(), "Entry cannot be empty.", Toast.LENGTH_LONG).show();
+
+        } else {
+
+            entry.setTitle(title);
+            entry.setText(text);
+
+            dbHandler.addEntry(entry);
+            dbHandler.close();
+
+            // Clear the form
+            titleEditText.setText("");
+            entryEditText.setText("");
+
+            // Exit new entry activity
+            finish();
+        }
     }
 
 }
