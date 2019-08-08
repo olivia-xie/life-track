61e73ab35d595567af8c05a12ea755162e3719cd
Olivia Xie <oxie@bdo.ca>
2019-08-08 15:09:16 -0400

    add lockscreen UI

--- app/src/main/java/com/example/newdiary/Activities/LockScreenActivity.java
+++ app/src/main/java/com/example/newdiary/Activities/LockScreenActivity.java
@@ -7,34 +7,49 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.andrognito.pinlockview.IndicatorDots;
+import com.andrognito.pinlockview.PinLockListener;
+import com.andrognito.pinlockview.PinLockView;
 import com.example.newdiary.Data.SharedPrefs;
 import com.example.newdiary.R;
 
 public class LockScreenActivity extends AppCompatActivity {
 
-    private EditText password;
-    private Button button;
+    private PinLockView pinLockView;
+    private IndicatorDots indicatorDots;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lock_screen);
 
-        password = findViewById(R.id.passwordEnterId);
-        button = findViewById(R.id.button);
+        final String actualPIN = getIntent().getStringExtra("actualPIN");
 
-        button.setOnClickListener(new View.OnClickListener() {
+        pinLockView = findViewById(R.id.pinLockViewId);
+        indicatorDots = findViewById(R.id.indicatorDotsId);
+
+        pinLockView.attachIndicatorDots(indicatorDots);
+        indicatorDots.setIndicatorType(IndicatorDots.IndicatorType.FILL_WITH_ANIMATION);
+
+        pinLockView.setPinLockListener(new PinLockListener() {
             @Override
-            public void onClick(View v) {
+            public void onComplete(String pin) {
 
-                String actualPIN = getIntent().getStringExtra("actualPIN");
-                String enteredPIN = password.getText().toString();
-
-                if (enteredPIN.equals(actualPIN)) {
+                if (pin.equals(actualPIN)){
                     finish();
                 } else {
-                    Toast.makeText(getApplicationContext(), "Incorrect PIN", Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), "Incorrect PIN.", Toast.LENGTH_LONG).show();
                 }
+            }
+
+            @Override
+            public void onEmpty() {
+
+            }
+
+            @Override
+            public void onPinChange(int pinLength, String intermediatePin) {
+
             }
         });
 
@@ -44,4 +59,5 @@ public class LockScreenActivity extends AppCompatActivity {
     public void onBackPressed() {
 
     }
+
 }
