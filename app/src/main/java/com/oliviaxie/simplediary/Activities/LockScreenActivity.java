fb4ad8d9b591b5adc16ce031a4eb23fbf8b4a232
Olivia Xie <olivia.xie@hotmail.com>
2020-05-09 14:38:48 -0400

    add fingerprint

--- app/src/main/java/com/oliviaxie/simplediary/Activities/LockScreenActivity.java
+++ app/src/main/java/com/oliviaxie/simplediary/Activities/LockScreenActivity.java
@@ -1,15 +1,23 @@
 package com.oliviaxie.simplediary.Activities;
 
+import android.Manifest;
 import android.app.AlertDialog;
+import android.app.KeyguardManager;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.hardware.biometrics.BiometricPrompt;
 import android.os.Bundle;
+import android.os.CancellationSignal;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.andrognito.pinlockview.IndicatorDots;
@@ -17,11 +25,18 @@ import com.andrognito.pinlockview.PinLockListener;
 import com.andrognito.pinlockview.PinLockView;
 import com.oliviaxie.simplediary.R;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
 public class LockScreenActivity extends AppCompatActivity {
 
     private PinLockView pinLockView;
     private IndicatorDots indicatorDots;
     private TextView promptText;
+    private Button useFingerprintButton;
+    private CancellationSignal cancellationSignal;
+    private Executor executor;
+    private BiometricPrompt biometricPrompt;
 
     private SharedPreferences prefs;
 
@@ -39,13 +54,21 @@ public class LockScreenActivity extends AppCompatActivity {
 
         final String actualPIN = prefs.getString("pin", null);
 
+        executor = Executors.newSingleThreadExecutor();
+
         pinLockView = findViewById(R.id.pinLockViewId);
         indicatorDots = findViewById(R.id.indicatorDotsId);
         promptText = findViewById(R.id.promptTextId);
+        useFingerprintButton = findViewById(R.id.useBiometricButtonId);
 
         // Attaching indicator dots to pin lock view
         pinLockView.attachIndicatorDots(indicatorDots);
         indicatorDots.setIndicatorType(IndicatorDots.IndicatorType.FIXED);
+
+        // Disabling button if biometric scanning is not supported by device
+        if (!checkBiometricSupport()) {
+            useFingerprintButton.setEnabled(false);
+        }
 
         // Lock screen pin pad listener
         pinLockView.setPinLockListener(new PinLockListener() {
@@ -71,6 +94,28 @@ public class LockScreenActivity extends AppCompatActivity {
             @Override
             public void onPinChange(int pinLength, String intermediatePin) {
 
+            }
+        });
+
+        // Use biometric authentication
+        useFingerprintButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                biometricPrompt = new BiometricPrompt.Builder(LockScreenActivity.this)
+                        .setTitle("Authenticate Using Fingerprint")
+                        .setSubtitle("")
+                        .setDescription("")
+                        .setNegativeButton("Cancel", executor,
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                    }
+                                })
+                        .build();
+
+                biometricPrompt.authenticate(getCancellationSignal(), executor,
+                        getAuthenticationCallback());
             }
         });
     }
@@ -101,6 +146,72 @@ public class LockScreenActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private Boolean checkBiometricSupport() {
+        KeyguardManager keyguardManager =
+                (KeyguardManager) getSystemService(KEYGUARD_SERVICE);
+
+        PackageManager packageManager = this.getPackageManager();
+
+        if (!keyguardManager.isKeyguardSecure()) {
+            return false;
+        }
+
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.USE_BIOMETRIC) !=
+                PackageManager.PERMISSION_GRANTED) {
+            return false;
+        }
+
+        if (packageManager.hasSystemFeature(PackageManager.FEATURE_FINGERPRINT))
+        {
+            return true;
+        }
+
+        return true;
+    }
+
+    private BiometricPrompt.AuthenticationCallback getAuthenticationCallback() {
+
+        return new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode, CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+            }
+
+            @Override
+            public void onAuthenticationHelp(int helpCode, CharSequence helpString) {
+                super.onAuthenticationHelp(helpCode, helpString);
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(
+                    BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+
+                // Start main activity after successful authentication
+                Intent intent = new Intent(LockScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+    }
+
+    private CancellationSignal getCancellationSignal() {
+        cancellationSignal = new CancellationSignal();
+        cancellationSignal.setOnCancelListener(
+                new CancellationSignal.OnCancelListener() {
+                    @Override
+                    public void onCancel() {
+                    }
+                });
+        return cancellationSignal;
     }
 
     public void setColorTheme(String themePref) {
